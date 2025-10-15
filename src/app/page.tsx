ec3e13ac075532131acd65083377366b4735cf87
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-10-15 16:28:37 +0300

    feat: fully functionnal carousel with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-import Navbar from "@/components/common/Navbar";
-import Image from "next/image";
+import assets from "@/assets/images/assets";
+import Carousel from "@/components/carousel/Carousel";
+import Slide from "@/components/carousel/Slide";
+
+const slides = [
+  <Slide
+    image={assets.heroIllus1}
+    title="Produit populaire"
+    price="29 900 Ar"
+    key={1}
+  />,
+  <Slide
+    image={assets.heroIllus2}
+    title="Produit populaire"
+    price="29 900 Ar"
+    key={2}
+  />,
+  <Slide
+    image={assets.heroIllus3}
+    title="Produit populaire"
+    price="29 900 Ar"
+    key={3}
+  />,
+];
 
 export default function Home() {
   return (
-    <div className="">
-      <Navbar />
+    <div className="min-h-screen px-4">
+      <div className="lg:px-50">
+        <Carousel slides={slides} />
+      </div>
     </div>
   );
 }
--- src/assets/images/assets.ts
+++ src/assets/images/assets.ts
@@ -1,7 +1,13 @@
 import logoLight from "./logo-light.png";
+import heroIllus1 from "./heroIllus1.jpg";
+import heroIllus2 from "./heroIllus2.jpg";
+import heroIllus3 from "./heroIllus3.jpg";
 
 const assets = {
   logoLight,
+  heroIllus1,
+  heroIllus2,
+  heroIllus3,
 };
 
 export default assets;
