d0b006e707d7bedaf65e704990e4059ccaa4c80f
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-10-16 11:02:33 +0300

    feat: trending category card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import assets from "@/assets/images/assets";
 import Carousel from "@/components/carousel/Carousel";
 import Slide from "@/components/carousel/Slide";
+import CategoryRounded from "@/components/home/CategoryRounded";
 import MiniCategoryCard from "@/components/home/MiniCategoryCard";
+import SectionWrapper from "@/components/home/SectionWrapper";
 
 const slides = [
   <Slide
@@ -24,7 +26,16 @@ const slides = [
   />,
 ];
 
-const category = [
+const categoryOverview = [
+  { title: "Vêtements", image: assets.clothesCategory },
+  { title: "Chaussures", image: assets.shoesCategory },
+  { title: "Accessoires", image: assets.accessoriesCategory },
+  { title: "Électronique", image: assets.electronicsCategory },
+  { title: "Maison", image: assets.houseCategory },
+  { title: "Beauté", image: assets.beautyCategory },
+];
+
+const popularCategories = [
   { title: "Vêtements", image: assets.clothesCategory },
   { title: "Chaussures", image: assets.shoesCategory },
   { title: "Accessoires", image: assets.accessoriesCategory },
@@ -35,10 +46,10 @@ const category = [
 
 export default function Home() {
   return (
-    <div className="min-h-screen px-4">
+    <div className="min-h-screen px-4 w-full flex flex-col">
       <div className="md:px-10 md:grid md:grid-cols-10 w-full gap-5 ">
         <div className="col-span-2 flex flex-col gap-4">
-          {category.slice(0, 3).map((cat, index) => (
+          {categoryOverview.slice(0, 3).map((cat, index) => (
             <MiniCategoryCard key={index} title={cat.title} image={cat.image} />
           ))}
         </div>
@@ -46,11 +57,19 @@ export default function Home() {
           <Carousel slides={slides} />
         </div>
         <div className="col-span-2 flex flex-col gap-4">
-          {category.slice(3).map((cat, index) => (
+          {categoryOverview.slice(3).map((cat, index) => (
             <MiniCategoryCard key={index} title={cat.title} image={cat.image} />
           ))}
         </div>
       </div>
+
+      <SectionWrapper title="Catégories populaires" className="mt-5 px-20!">
+        <div className="flex justify-between">
+          {popularCategories.map(({ title, image }) => (
+            <CategoryRounded label={title} image={image} key={title} />
+          ))}
+        </div>
+      </SectionWrapper>
     </div>
   );
 }
